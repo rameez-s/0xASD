d5d8cb8ee52568bff58dc13a0b091cb5bd909d66
Isak Wahlqvist <isak.wahlqvist@gmail.com>
2017-02-13 19:04:25 +0300

    Added files to start engine

---
+++ src/engine/Engine.java
@@ -0,0 +1,7 @@
+package engine;
+
+/**
+ * Created by 18iwahlqvist on 2/12/2017.
+ */
+public class Engine {
+}
---
+++ src/engine/rendering/Shader.java
@@ -0,0 +1,7 @@
+package engine.rendering;
+
+/**
+ * Created by 18iwahlqvist on 2/12/2017.
+ */
+public class Shader {
+}
