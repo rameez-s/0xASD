e18b6cc7383151d9930e92a73ad607cf4d3443a6
17aperezcaceres <17aperezcaceres@asd.edu.qa>
2017-05-10 13:53:40 +0300

    Fixed scene switcher

---
+++ src/objects/npcs/PeTeacher.java
@@ -0,0 +1,19 @@
+package objects.npcs;
+
+        import engine.animation.Animation;
+        import engine.math.Vector2f;
+        import engine.math.Vector3f;
+        import engine.objects.Sprite;
+        import objects.NPC;
+
+        import java.util.ArrayList;
+
+/**
+ * Created by 17aperezcaceres on 5/8/2017.
+ */
+public class PeTeacher extends NPC {
+    public PeTeacher () {
+        super(128);
+        setTexture("characterSheet.png");
+    }
+}
--- src/scenes/Gym.java
+++ src/scenes/Gym.java
@@ -1,12 +1,17 @@
 package scenes;
 
 import engine.objects.Scene;
+import engine.math.Vector2f;
+import engine.objects.Scene;
+import engine.objects.Sprite;
 
+import java.util.ArrayList;
 /**
  * Created by 18iwahlqvist on 4/20/2017.
  */
 public class Gym extends Scene {
     //Test
+
     public Gym(){
         setMap("gym.png");
         genMap();
