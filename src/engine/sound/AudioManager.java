6b9adacebfac5d6e611acd3461838dbe0a41f5f4
18iwahlqvist <isak.wahlqvist@gmail.com>
2017-05-21 15:07:56 +0300

    Finished sound

--- src/engine/sound/AudioManager.java
+++ src/engine/sound/AudioManager.java
@@ -1,6 +1,22 @@
 package engine.sound;
+//
+import engine.VolatileManager;
+import org.lwjgl.*;
+import org.lwjgl.openal.*;
+import org.lwjgl.stb.*;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+import static engine.util.IOUtil.ioResourceToByteBuffer;
+import static org.lwjgl.openal.AL10.*;
+import static org.lwjgl.openal.ALC10.*;
+import static org.lwjgl.openal.ALC11.*;
+import static org.lwjgl.openal.EXTThreadLocalContext.*;
+import static org.lwjgl.stb.STBVorbis.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
 
 /**
  * Created by Isak Wahlqvist on 4/30/2017.
@@ -8,21 +24,132 @@ import java.util.ArrayList;
 public class AudioManager {
     //Test
 
+
+
+
+
+    public static long audioDevice, context;
     private ArrayList<Sound> sounds = new ArrayList<>();
 
-    public AudioManager(){
+    public AudioManager() {
+        long device = alcOpenDevice((ByteBuffer)null);
+        if (device == NULL) {
+            throw new IllegalStateException("Failed to open the default device.");
+        }
 
-    }
+        ALCCapabilities deviceCaps = ALC.createCapabilities(device);
 
-    public void add(Sound s){
-        sounds.add(s);
-    }
 
-    public void play(String soundName){
-        for(Sound s : sounds){
-            if(s.name.equalsIgnoreCase(soundName)){
+        System.out.println("OpenALC10: " + deviceCaps.OpenALC10);
+        System.out.println("OpenALC11: " + deviceCaps.OpenALC11);
+        System.out.println("caps.ALC_EXT_EFX = " + deviceCaps.ALC_EXT_EFX);
 
+        if (deviceCaps.OpenALC11) {
+            List<String> devices = ALUtil.getStringList(NULL, ALC_ALL_DEVICES_SPECIFIER);
+            if (devices == null) {
+            } else {
+                for (int i = 0; i < devices.size(); i++) {
+                    System.out.println(i + ": " + devices.get(i));
+                }
             }
         }
+
+        String defaultDeviceSpecifier = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+        System.out.println("Default device: " + defaultDeviceSpecifier);
+
+        long context = alcCreateContext(device, (IntBuffer)null);
+        alcSetThreadContext(context);
+        AL.createCapabilities(deviceCaps);
+
+        System.out.println("ALC_FREQUENCY: " + alcGetInteger(device, ALC_FREQUENCY) + "Hz");
+        System.out.println("ALC_REFRESH: " + alcGetInteger(device, ALC_REFRESH) + "Hz");
+        System.out.println("ALC_SYNC: " + (alcGetInteger(device, ALC_SYNC) == ALC_TRUE));
+        System.out.println("ALC_MONO_SOURCES: " + alcGetInteger(device, ALC_MONO_SOURCES));
+        System.out.println("ALC_STEREO_SOURCES: " + alcGetInteger(device, ALC_STEREO_SOURCES));
+
+        try {
+            while (true) {
+                if(VolatileManager.create){
+                    VolatileManager.soundToPlay = new Sound(VolatileManager.location);
+                    VolatileManager.create = false;
+                }
+                if(VolatileManager.play) {
+                    if(VolatileManager.soundToPlay.source == -1 || VolatileManager.soundToPlay.buffer == -1){
+                        VolatileManager.soundToPlay.buffer = alGenBuffers();
+                        VolatileManager.soundToPlay.source = alGenSources();
+
+                        try (STBVorbisInfo info = STBVorbisInfo.malloc()) {
+                            ShortBuffer pcm = AudioManager.readVorbis("res/audio/" + VolatileManager.soundToPlay.fileName, 32 * 1024, info);
+
+                            //copy to buffer
+                            alBufferData(VolatileManager.soundToPlay.buffer, info.channels() == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, pcm, info.sample_rate());
+                        }
+
+                        alSourcei(VolatileManager.soundToPlay.source, AL_BUFFER, VolatileManager.soundToPlay.buffer);
+                        alSourcei(VolatileManager.soundToPlay.source, AL_LOOPING, AL_TRUE);
+                    }
+                    alSourcePlay(VolatileManager.soundToPlay.source);
+                    if (VolatileManager.soundToPlay.length != -1) {
+                        Thread.sleep(VolatileManager.soundToPlay.length);
+                    } else {
+                        Thread.sleep(4000);
+                    }
+                    System.out.println("started");
+                    alSourceStop(VolatileManager.soundToPlay.source);
+                }else{
+                    System.out.println("stopped");
+                }
+            }
+        }catch(Exception e){} finally {
+            alcMakeContextCurrent(NULL);
+            alcDestroyContext(context);
+            alcCloseDevice(device);
+        }
     }
+
+    public static void main(String args[]){
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AudioManager am = new AudioManager();
+            }
+        });
+        VolatileManager.play = false;
+        VolatileManager.soundToPlay = new Sound("Example.ogg");
+        VolatileManager.play = true;
+        t.start();
+    }
+
+
+
+
+
+    static ShortBuffer readVorbis(String resource, int bufferSize, STBVorbisInfo info) {
+        ByteBuffer vorbis;
+        try {
+            vorbis = ioResourceToByteBuffer(resource, bufferSize);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        IntBuffer error   = BufferUtils.createIntBuffer(1);
+        long      decoder = stb_vorbis_open_memory(vorbis, error, null);
+        if (decoder == NULL) {
+            throw new RuntimeException("Failed to open Ogg Vorbis file. Error: " + error.get(0));
+        }
+
+        stb_vorbis_get_info(decoder, info);
+
+        int channels = info.channels();
+
+        int lengthSamples = stb_vorbis_stream_length_in_samples(decoder);
+
+        ShortBuffer pcm = BufferUtils.createShortBuffer(lengthSamples);
+
+        pcm.limit(stb_vorbis_get_samples_short_interleaved(decoder, channels, pcm) * channels);
+        stb_vorbis_close(decoder);
+
+        return pcm;
+    }
+
 }
--- src/engine/sound/Sound.java
+++ src/engine/sound/Sound.java
@@ -1,106 +1,21 @@
 package engine.sound;
 
-import engine.util.IOUtil;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.openal.AL10;
-import org.lwjgl.stb.STBVorbisInfo;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.nio.ShortBuffer;
-
-import static org.lwjgl.openal.AL10.*;
-import static org.lwjgl.stb.STBVorbis.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
-
 /**
  * Created by Isak Wahlqvist on 4/30/2017.
  */
 public class Sound {
-    public String name;
-    public int buffer, source;
-
-    public Sound(String name, String fileName, boolean loop){
-        boolean soundExists = false;
-        for(Sound s : AllSounds.sounds){
-            if(s.name.equals(name)){
-                soundExists = true;
-            }
-        }
-        //Test
-        if(soundExists){
-            System.out.println("Sound already exists");
-        }else {
-            this.name = name;
-
-            buffer = alGenBuffers();
-
-            source = alGenBuffers();
-
-
-            try (STBVorbisInfo info = STBVorbisInfo.malloc()) {
-                ShortBuffer pcm = readVorbis("/res/sounds/" + fileName, 32 * 1024, info);
-
-                alBufferData(buffer, info.channels() == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, pcm, info.sample_rate());
-
-            }
-
-            alSourcei(source, AL_BUFFER, buffer);
-
-            if (loop) {
-                alSourcei(source, AL_LOOPING, AL_TRUE);
-            } else {
-                alSourcei(source, AL_LOOPING, AL_FALSE);
-            }
-
-            AllSounds.sounds.add(this);
-        }
+    public int buffer = -1, source = -1;
+    public int length = -1;
+    public static Sound hit, song;
+    public String fileName;
+    public static void init(){
+        hit = new Sound("hit.ogg");
+        song = new Sound("song.ogg");
+        hit.length = 100;
+        song.length = 5000;
     }
-
-
-    public void play(){
-        alSourcePlay(source);
-    }
-
-    public void stop(){
-        alSourceStop(source);
-    }
-
-    public void delete(){
-        alDeleteSources(source);
-        alDeleteBuffers(buffer);
-    }
-
-
-
-    static ShortBuffer readVorbis(String resource, int bufferSize, STBVorbisInfo info) {
-        ByteBuffer vorbis;
-        try {
-            vorbis = IOUtil.ioResourceToByteBuffer(resource, bufferSize);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        IntBuffer error = BufferUtils.createIntBuffer(1);
-        long decoder = stb_vorbis_open_memory(vorbis, error, null);
-        if ( decoder == NULL )
-            throw new RuntimeException("Failed to open Ogg Vorbis file. Error: " + error.get(0));
-
-        stb_vorbis_get_info(decoder, info);
-
-        int channels = info.channels();
-
-        int lengthSamples = stb_vorbis_stream_length_in_samples(decoder);
-
-        ShortBuffer pcm = BufferUtils.createShortBuffer(lengthSamples);
-
-        pcm.limit(stb_vorbis_get_samples_short_interleaved(decoder, channels, pcm) * channels);
-        stb_vorbis_close(decoder);
-
-        return pcm;
+    public Sound(String fileName){
+        this.fileName = fileName;
     }
 
 }
